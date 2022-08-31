71f7feb8a32df980ccfba9a27c78cf2e7443adc4
Leo-Kopec <lkopec4@gmail.com>
2022-08-31 11:42:35 -0400

    Added savings-api URLs to Environments

--- spyglass-ui/src/environments/environment.ts
+++ spyglass-ui/src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  api: {
+    root: 'http://localhost:8080/',
+    goals: 'goals/',
+    users: 'users/'
+  }
 };
 
 /*
