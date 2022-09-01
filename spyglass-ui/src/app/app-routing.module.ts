779f61049d4dff8874ad9ed36418e91da4240a26
Leo-Kopec <lkopec4@gmail.com>
2022-09-01 13:32:24 -0400

    Created Navbar Component

--- spyglass-ui/src/app/app-routing.module.ts
+++ spyglass-ui/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DisplayGoalsComponent } from './components/display-goals/display-goals.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'homepage',
+  component: HomepageComponent
+}, {
+  path: 'goals',
+  component: DisplayGoalsComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
